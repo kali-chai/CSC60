f03e6827fe4bb09ae12b0c8a5fd35350597529d6
kali-chai <kali.chai@outlook.com>
2026-01-30 13:52:40 -0800

    Lab 2 work.

---
+++ Lab2/lab2.c
@@ -0,0 +1,17 @@
+/* ----------------
+ * Kali Wilson
+ * Lab 2            */
+
+/* Preprocessor Directives */
+#include <stdio.h>
+#include <stdlib.h>	
+
+/* Main function (entry point) */
+int main(void) {
+	printf("\nLab 2.\n\n");
+	printf("Hello World\n");
+	printf("Hi, Kali.\n\n");
+	printf("\"The greatest crimes arise not from necessity, but from excess.\"\n");
+	/* Exit with status code 0 */
+	return EXIT_SUCCESS;
+}
